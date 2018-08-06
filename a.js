bc9c085b092844c68507572804ffb838aaa28a85
wt <307548594@qq.com>
2018-08-06 15:16:52 +0800

    阿里测评

---
+++ a.js
@@ -0,0 +1,5 @@
+var s=1;
+for(var i=2;i<=10;i++){
+    s=s*(1-1/(i*i));
+}
+console.log(s);
---
+++ pdd.js
@@ -0,0 +1,37 @@
+// var arr=[0,1,2,3,4,5,6,7,8,9,8];
+// arr.sort();
+// console.log(arr);
+// function repeat(arr){
+//     var newArr=[];
+//     arr.sort(function(a,b){
+//         console.log(a,b);
+//         if(a===b && newArr.indexOf(a)===-1){
+//             newArr.push(a);
+//             console.log(newArr);
+//         }
+//     });
+//     return newArr;
+// }
+
+// var result=repeat(arr);
+// console.log(result);
+
+
+//var arr=[1,2,2,2,3,2,5,5,3,1];
+// function f(arr){
+//     var set = new Set(arr)
+//     return [...set];
+// }
+// console.log(f(arr)); //[ 1, 2, 3, 5 ]
+
+//console.log(typeof null);// object
+
+
+// var a={};
+// b={key: 'b'};
+// c={key:'c'};
+// a[b]=123;
+// a[c]=456;
+// console.log(a[b]);//456
+// console.log(a[b]===a[c]);//true
+
